9d1f26c9798f06b378daae3bacb2d675d6ed8bfc
bactoria <bactoria@gmail.com>
2019-02-04 23:55:04 +0900

    Fix typo

--- Node/1. module.js
+++ Node/1. module.js
@@ -1,16 +1,14 @@
 // 기존
-const a = require('./module_env');
+const a = require('./1. module_env');
 console.log(a.id);
 console.log(a.name);
 
 // ES6
-const {id, name} = require('./module_env');
+const {id, name} = require('./1. module_env');
 console.log(id);
 console.log(name);
 
 
-
-//export가 함수1개라서 함수이름은 변경할 수 있음.
-const isOdddddd = require('./1. module_env2');
-console.log(isOdddddd(8));
-
+//함수 이름은 변경할 수 있음.
+const isOdd= require('./1. module_env2');
+console.log(isOdd(8));
--- Node/4. Timer.js
+++ Node/4. Timer.js
@@ -1,5 +1,5 @@
 
-/*setTimeout*/
+/*  setTimeout  */
 // 2초 후에 실행
 const timeout_2s = setTimeout(() => {
     console.log('2초 후에 실행');
@@ -10,7 +10,7 @@ const timeout_4s = setTimeout(() => {
     console.log('4초 후에 실행');
 }, 4000);
 
-/*setInterval*/
+/*  setInterval  */
 // 1초 마다 실행
 const interval_1s = setInterval(() => {
     console.log('1초 마다 실행')
@@ -27,7 +27,8 @@ const asd = setTimeout(() => {
 1초 마다 실행
 */
 
-/*setImmediate*/
+
+/*  setImmediate  */
 const im = setImmediate(() => console.log('즉시 실행'));
 // Q :: const im = console.log('즉시 실행'); 과 다른점
 // A :: 위의 경우는 이벤트 루프로 보내기 때문에 비동기 처리가 가능함. 아래는 동기.
